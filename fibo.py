6bce96022800f93daa8eb7d22c92eda08df6c191
nataliemartincova <naty334@seznam.cz>
2025-04-24 10:03:43 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,19 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    n = int(input("Pocet clenu Fibonacciho posloupnosti: "))
+    sek = []
+    for num in range(n):
+        sek.append(recursive_nth_fibo(num))
+
+    print(sek)
 
 
 if __name__ == "__main__":
